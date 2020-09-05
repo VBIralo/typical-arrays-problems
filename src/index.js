62eebe41af3892b47b0eafb348cbe925e2540acf
VBiralo <mr.biralo@yandex.ru>
2020-09-06 01:02:44 +0300

    feat: add max, min, and avg functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(arr) {
+    if (!arr || arr.length == 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
+    let min = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i]
+    };
+
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr) {
+    if (!arr || arr.length == 0) return 0;
+
+    let max = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) max = arr[i]
+    };
+
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr) {
+    if (!arr || arr.length == 0) return 0;
+
+    let sum = 0;
+    arr.map(n => sum += n)
+
+    return sum / arr.length
 }
